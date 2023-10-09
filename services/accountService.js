d3734c8f78b6103068d2762b8ffe276f64d3468d
BjarniGod <bgjonsson@mail.fhsu.edu>
2023-10-09 15:05:07 -0500

    removed outdated files

--- services/accountService.js
+++
@@ -1,8 +0,0 @@
-import * as API from '../models/api.js';
-
-export function getAccountDetails(accountType) {
-    return API.fetchAccount(accountType)
-        .then(account => {
-            return account;
-        });
-}
