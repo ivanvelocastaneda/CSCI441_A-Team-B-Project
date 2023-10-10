9aca2430e7be052d2bf1369ae53cb234c647cd1c
bgjonsson@mail.fhsu.edu <bgjonsson@mail.fhsu.edu>
2023-10-10 16:24:56 -0500

    updated javascript for main.js and api.js

--- models/api.js
+++ models/api.js
@@ -1,34 +1,3 @@
-// const BASE_URL = "https://csci441-teamb.onrender.com";
-
-
-// export const getMenuItems = async () => {
-//     const response = await fetch(`${BASE_URL}/menu_item`);
-//     // console.log(response.json());
-//     return response.json();
-// };
-
-// export const getMenuItemById = async (id) => {
-//     const response = await API.fetch(`${BASE_URL}/menu_item/${id}`);
-//     return response.json();
-// };
-
-// export const createMenuItem = async (menuItem) => {
-//     const response = await API.post(`${BASE_URL}/menu_item`, menuItem);
-//     return response.json();
-// };
-
-// export const updateMenuItem = async (id, updatedData) => {
-//     const response = await API.put(`${BASE_URL}/menu_item/${id}`, updatedData);
-//     return response.json();
-// };
-
-// export const deleteMenuItem = async (id) => {
-//     const response = await API.delete(`${BASE_URL}/menu-item/${id}`);
-//     return response.json();
-// };
-
-
-
 const BASE_URL = "https://csci441-teamb.onrender.com";
 
 export async function fetchMenuItems() {
@@ -38,5 +7,3 @@ export async function fetchMenuItems() {
     }
     return await response.json();
 }
-
-
