617cab2cfa0865c625ff9a78f8e060bbdf821729
bgjonsson@mail.fhsu.edu <bgjonsson@mail.fhsu.edu>
2023-10-27 07:38:50 -0500

    Items dynamically added to homepage

--- js/featuredMenuItems.js
+++ js/featuredMenuItems.js
@@ -2,32 +2,35 @@ import { MenuItemController } from '../controllers/menuItemController.js';
 import { MenuItem } from '../models/menuItem.js';
 
 class View {
-    // New function to implement
+
     addMenuItem(menuItem) {
-        const section = document.getElementById('featured-menu');
-        const div = document.createElement('div');
-        div.id = `featured-item-${menuItem.itemID}`;
-        div.className = 'menu-item';
+        const cards = document.getElementById('cards');
+        const foodcard = document.createElement('div');
+        foodcard.id = `featured-item-${menuItem.itemID}`;
+        foodcard.className = 'foodcard';
 
         const img = document.createElement('img');
         img.alt = menuItem.itemName;
         img.src = menuItem.itemImage;
 
         const h3 = document.createElement('h3');
+        h3.className = 'name';
         h3.textContent = menuItem.itemName;
 
         const p = document.createElement('p');
+        p.className = 'description';
         p.textContent = menuItem.description;
 
         const span = document.createElement('span');
         span.className = 'price';
         span.textContent = `$${menuItem.price}`;
 
-        div.appendChild(img);
-        div.appendChild(h3);
-        div.appendChild(p);
-        div.appendChild(span);
-        section.appendChild(div);
+        foodcard.appendChild(img);
+        foodcard.appendChild(h3);
+        foodcard.appendChild(p);
+        foodcard.appendChild(span);
+        cards.appendChild(foodcard);
+        console.log(menuItem);
     }
 
 }
