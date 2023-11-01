6d9fac394f245d9b290ebe707eabd4afdf3b63d0
bgjonsson@mail.fhsu.edu <bgjonsson@mail.fhsu.edu>
2023-11-01 14:26:38 -0500

    Loading spinner working on customer page

--- controllers/featuredItemsController.js
+++ controllers/featuredItemsController.js
@@ -10,24 +10,24 @@ export class FeaturedMenuItemController {
     async init() {
         const data = await fetchMenuItems();
         data.forEach(item => {
-            const menuItem = new this.model(item.itemID, item.itemName, item.description, item.price, item.itemImage);
-            this.view.addFeaturedItem(menuItem);
+            // Temporary featured items selector
+            if (item.itemID === 6 || item.itemID === 7 || item.itemID === 10) {
+                const menuItem = new this.model(item.itemID, item.itemName, item.description, item.price, item.itemImage);
+                this.view.addFeaturedItem(menuItem);
+            }
         });
+        // Loading spinner removed when function is done loading
+        document.getElementById('loading').style.display = 'none';
 
     }
 
     async addFeaturedItem(data) {
-        // Spinner not working
-        this.view.showSpinner();
         try {
-            setTimeout(resolve, 3000);
             const newItem = await this.createMenuItem(data);
             const menuItem = new this.model(newItem.itemID, newItem.itemName, newItem.description, newItem.price, newItem.itemImage);
             this.view.addFeaturedItems(menuItem);
         } catch (error) {
             console.error('Failed to add featured item: ', error);
-        } finally {
-            this.view.hideSpinner();
         }
     }
 }
