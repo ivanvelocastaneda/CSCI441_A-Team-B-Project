04c537feeca574fbf2a6be587d2a6af935e2b3a6
bgjonsson@mail.fhsu.edu <bgjonsson@mail.fhsu.edu>
2023-11-29 17:55:17 -0600

    Updated orders class

--- models/orders.js
+++ models/orders.js
@@ -1,25 +1,27 @@
 class Orders {
-    constructor(orderID, customerID, orderStatus, created_at, updated_at) {
+    constructor(orderID, orderStatus, created_at, updated_at, restaurantTable) {
         this.orderID = orderID;
-        this.customerID = customerID;
         this.orderStatus = orderStatus;
+        this.menuItems - menuItems;
         this.created_at = created_at;
         this.updated_at = updated_at;
+        this.restaurantTable = restaurantTable;
     }
 
     update(data) {
         this.orderID = data.orderID || this.orderID;
-        this.customerID = data.customerID || this.customerID;
         this.orderStatus = data.orderStatus || this.orderStatus;
+        this.menuItems = data.menuItems || this.menuItems;
         this.created_at = data.created_at || this.created_at;
         this.updated_at = data.updated_at || this.updated_at;
+        this.restaurantTable = data.restaurantTable || this.restaurantTable;
     }
 
     getOrderStatus() {
         return this.orderStatus;
     }
 
-    updateOrderStatus(newStatus) {
-        this.orderStatus = newStatus;
+    getMenuItems() {
+        return this.menuItems
     }
 }
