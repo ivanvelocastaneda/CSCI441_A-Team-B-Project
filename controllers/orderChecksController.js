3f3438b00fbe8239e758440fa53122518e2c2413
bgjonsson@mail.fhsu.edu <bgjonsson@mail.fhsu.edu>
2023-12-05 09:40:26 -0600

    Updated order checks page to show order items

--- controllers/orderChecksController.js
+++ controllers/orderChecksController.js
@@ -1,5 +1,5 @@
 
-import { deleteOrder, fetchOrders, updateOrder } from "../models/api.js";
+import { deleteOrder, fetchOrders, updateOrder, fetchAllOrderItems, fetchMenuItem, fetchFullOrder } from "../models/api.js";
 
 export class OrdersController {
     constructor(view) {
@@ -10,14 +10,31 @@ export class OrdersController {
     async init() {
         try {
             const data = await fetchOrders();
-            data.forEach((order) => {
-                this.view.createListItem(order);
+            data.forEach(async (order) => {
+                const items = await fetchFullOrder(order.orderID);
+                this.view.createListItem(order, items);
             });
         } catch (error) {
             console.log("Not able to fetch orders: ", error);
         } finally {
             // Loading spinner removed when function is done loading
             document.getElementById('loading').style.display = 'none';
+        }
+    }
+
+    async getOrderItems(orderID) {
+        try {
+            return await fetchAllOrderItems();
+        } catch (error) {
+            console.error('Error fetching menu items for orderID: ', orderID);
+        } 
+    }
+
+    async getMenuItem(itemID) {
+        try {
+            return await fetchMenuItem(itemID);
+        } catch (err) {
+            console.error('Failed to fetch menu item: ', itemID)
         }
     }
 
--- js/orderChecks.js
+++ js/orderChecks.js
@@ -2,13 +2,11 @@ import { OrdersController } from "../controllers/orderChecksController.js";
 import { Orders } from "../models/orders.js";
 
 class View {
-    createListItem(order) {
+    createListItem(order, items) {
         const ordersContainer = document.getElementById('orders');
         const orderElement = document.createElement('div');
         orderElement.id = `order-${order.orderID}`;
         orderElement.className = 'order';
-        // orderElement.classList.add('order');
-        // orderElement.setAttribute('data-order-id', order.orderID); 
 
         const orderTitle = document.createElement('h2');
         orderTitle.className = 'order-title';
@@ -20,36 +18,30 @@ class View {
         orderStatus.textContent = `Order Status: ${order.orderStatus}`;
         orderElement.appendChild(orderStatus);
 
+        const createdAt = document.createElement('p');
+        createdAt.className = 'order-created';
+        createdAt.textContent = `Created at: ${order.created_at}`;
+        orderElement.appendChild(createdAt);
+
         const showDetailsButton = document.createElement('button');
         // showDetailsButton.classList.add('show-details');
         showDetailsButton.className = 'show-order-details';
         showDetailsButton.textContent = 'Show Details';
         showDetailsButton.style.cursor = 'pointer';
 
-        showDetailsButton.addEventListener('click', () => this.toggleOrderDetails(order, orderElement));
+        showDetailsButton.addEventListener('click', () => this.toggleOrderDetails(items, orderElement));
         
         orderElement.appendChild(showDetailsButton);
-
-        // const details = document.createElement('div');
-        // details.className = 'order-details';
-        // details.classList.add('details', 'hidden');
-
-        // const detailsContent = document.createElement('p');
-        // detailsContent.innerHTML = `<strong>Details:</strong><br>`;
-        // order.details.forEach((item) => {
-        //     detailsContent.innerHTML += `Item: ${item.itemName} (Quantity: ${item.quantity})<br>`;
-        // });
-        // detailsContent.innerHTML += `Special Request: ${order.specialRequest}<br>`;
-        // detailsContent.innerHTML += `Order Time: ${order.orderTime}<br>`;
-        // detailsContent.innerHTML += `Order Total: $${order.orderTotal.toFixed(2)}`;
-        // details.appendChild(detailsContent);
-
-        // orderElement.appendChild(details);
-
         ordersContainer.appendChild(orderElement);
     }
 
-    toggleOrderDetails(order, listItem) {
+    helperFunction(items, orderElement) {
+        items.forEach((item) => {
+            this.toggleOrderDetails(item, orderElement);
+        })
+    }
+
+    toggleOrderDetails(items, listItem) {
         let detailsDiv = listItem.querySelector('.details');
         if (!detailsDiv) {
             // Create details div if it doesn't exist
@@ -71,8 +63,9 @@ class View {
             };
     
             // Insert rows into the table
-            insertDetailRow(detailsTable, 'Created At', order.created_at);
-            insertDetailRow(detailsTable, 'Updated At', order.updated_at);
+            items.forEach((item) => {
+                insertDetailRow(detailsTable, item.itemName, 'Qty: ' + item.itemQuantity);
+            })
     
             detailsDiv.appendChild(detailsTable);
             detailsDiv.style.display = 'none';
@@ -128,10 +121,6 @@ document.addEventListener("DOMContentLoaded", function () {
             details.classList.toggle("hidden");
         });
     });
-
-    
-    // There is a variable on the order data called created_at that you can implement somewhere in the code to sort.
-    // I'm not sure right now where would be the best place to implement it but you might have some ideas.
 
 
 });
